6d610d0c8fba4b6d26da3591a62e7ed914c93524
carlosfelipepg@gmail.com <carlosfelipepg@gmail.com>
2023-10-25 12:32:41 -0300

    feat: add new fields news model

--- news/models.py
+++ news/models.py
@@ -13,6 +13,8 @@ class News(models.Model):
         ('entertainment', 'Entertainment'),
         ('technology', 'Technology')
     ))
+    created_at = models.DateTimeField(auto_now_add=True, null=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return self.title
